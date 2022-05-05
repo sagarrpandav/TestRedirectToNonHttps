4b3dae3bba5b58630438c592ed69e3406a7494a8
Sagar R Pandav <sagar.rpandav@gmail.com>
2022-05-05 09:32:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-setTimeout(() => {location.href = "http://hub.itp.io:5000/";}, 5000);
+const redirectToItp = () => {
+  setTimeout(() => {location.href = "http://hub.itp.io:5000/";}, 5000);
+};
+
+console.log("Hello");
